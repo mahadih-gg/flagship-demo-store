ebe7ccc32cf962bd97f47b72aa45b4da419d1798
mahadih <mahadih.dev@gmail.com>
2025-06-03 22:34:21 +0600

    Replace PNG images with WEBP format in the Product page for improved performance; update image references accordingly.

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -19,7 +19,7 @@ const Product = () => {
                 <GoChevronRight className="text-lg" />
               </button>
 
-              <img src="/assets/images/product/1.png" className="w-[200px] sm:w-[252px] lg:w-[332px] 2xl:w-[443px] h-auto" alt="product" />
+              <img src="/assets/images/product/1.webp" className="w-[200px] sm:w-[252px] lg:w-[332px] 2xl:w-[443px] h-auto" alt="product" />
             </div>
 
             <div>
@@ -39,7 +39,10 @@ const Product = () => {
           </div>
 
           <div className="flex flex-col gap-4">
-            <img src="/assets/images/product/2.png" alt="product" />
+            <img src="/assets/images/product/2.webp" className="hidden 2xl:block" alt="" />
+            <img src="/assets/images/product/lg/2.webp" className="hidden lg:block 2xl:hidden" alt="" />
+            <img src="/assets/images/product/md/2.webp" className="hidden sm:block lg:hidden" alt="" />
+            <img src="/assets/images/product/sm/2.webp" className="block sm:hidden" alt="" />
 
             <div>
               <h3 className="text-2xl text-[#1D1D1F] font-bold pt-8 pb-2.5">
@@ -59,11 +62,10 @@ const Product = () => {
 
       <section className="container section-padding">
         <div className="flex justify-center items-center">
-          <img src="/assets/images/product/3.png" className="hidden lg:block" alt="hero" />
-
-          <img src="/assets/images/product/md/3.png" className="hidden sm:block lg:hidden" alt="hero" />
-
-          <img src="/assets/images/product/sm/3.png" className="block sm:hidden" alt="hero" />
+          <img src="/assets/images/product/3.webp" className="hidden 2xl:block" alt="" />
+          <img src="/assets/images/product/lg/3.webp" className="hidden lg:block 2xl:hidden" alt="" />
+          <img src="/assets/images/product/md/3.webp" className="hidden sm:block lg:hidden" alt="" />
+          <img src="/assets/images/product/sm/3.webp" className="block sm:hidden" alt="" />
         </div>
 
       </section>
@@ -85,11 +87,10 @@ const Product = () => {
         <h2 className="section-heading">Why Buy Online?</h2>
 
         <div className="flex justify-center items-center">
-          <img src="/assets/images/product/4.png" className="hidden lg:block" alt="hero" />
-
-          <img src="/assets/images/product/md/4.png" className="hidden sm:block lg:hidden" alt="hero" />
-
-          <img src="/assets/images/product/sm/4.png" className="block sm:hidden" alt="hero" />
+          <img src="/assets/images/product/4.webp" className="hidden 2xl:block" alt="" />
+          <img src="/assets/images/product/lg/4.webp" className="hidden lg:block 2xl:hidden" alt="" />
+          <img src="/assets/images/product/md/4.webp" className="hidden sm:block lg:hidden" alt="" />
+          <img src="/assets/images/product/sm/4.webp" className="block sm:hidden" alt="" />
         </div>
 
       </section>
