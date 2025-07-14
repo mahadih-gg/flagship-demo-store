3fb6a7da0c0d8e3893ffe1fe8d3faefb8a3957ce
mahadih <mahadih.dev@gmail.com>
2025-07-14 19:30:24 +0600

    Add Aeonik font files and update App.css for font integration
    - Added multiple font files for the Aeonik font family, including Light, Regular, Medium, Bold, and Black weights.
    - Updated App.css to include @font-face declarations for the new Aeonik fonts.
    - Adjusted font-family usage in the telco section for consistency.
    - Enhanced styling in various sections for improved layout and responsiveness.

--- src/components/BlueTopBar.tsx
+++ src/components/BlueTopBar.tsx
@@ -5,11 +5,11 @@ type PropsType = {
   children?: React.ReactNode;
 }
 
-const BlueTopBar = ({ containerClass, text = "Demo Store • Powered by Horizon • Sign up for Free Trial", children }: PropsType) => {
+const BlueTopBar = ({ containerClass, text = "Powered by Horizon • Request Free Trial", children }: PropsType) => {
   return (
     <div className="bg-[#1F2544] h-9 sm:h-6 lg:h-8 2xl:h-11 flex items-center">
       <div className={containerClass}>
-        <p className="text-white font-medium text-center py-3 text-xs sm:text-sm md:text-base 2xl:text-lg">{children ?? text}</p>
+        <p className="text-white text-center py-3 text-xs sm:text-sm md:text-base 2xl:text-lg">{children ?? text}</p>
       </div>
     </div>
   );
