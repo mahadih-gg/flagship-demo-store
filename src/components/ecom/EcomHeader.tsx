fad0f75efdf16e3e7ce6a64ff59272b018dcfe0d
mahadih <mahadih.dev@gmail.com>
2025-07-14 19:46:03 +0600

    Add Neue Plak and TWKLausanne font files, update App.css for font integration

--- src/components/ecom/EcomHeader.tsx
+++ src/components/ecom/EcomHeader.tsx
@@ -9,7 +9,6 @@ const EcomHeader = ({ title, className }: EcomHeaderProps) => {
   return (
     <div className={cn("flex justify-between items-center pb-6 lg:pb-[45px] 2xl:pb-[60px]", className)}>
       <h2 className="text-xl md:text-2xl lg:text-3xl 2xl:text-[52px] font-semibold text-[#1F2544]">{title}</h2>
-      <a href="#" className="text-xs md:text-sm 2xl:text-base text-[#1F2544] font-semibold underline uppercase">SHOP ALL</a>
     </div>
   );
 };
